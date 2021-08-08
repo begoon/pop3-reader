e46509db7e14b3d524614844446a1e6281c502df
Alexander Demin <alexander@demin.ws>
2021-08-08 16:24:37 +0100

    Add initial version

---
+++ pop3_reader.py
@@ -0,0 +1,47 @@
+import poplib
+
+import click
+import tqdm
+
+FROM = "pop3_reader"
+
+
+@click.command()
+@click.option('--pop3_host', default='pop.gmail.com', required=True, type=str)
+@click.option('--username', required=True, type=str)
+@click.option('--password', prompt=True, required=True, type=str)
+@click.option('--mbox-file', default='inbox.mbox', required=True, type=str)
+@click.option('--commit', default=False, is_flag=True, type=bool)
+@click.option('--dry-run', default=False, is_flag=True, type=bool)
+def pop3(pop3_host, username, password, mbox_file, commit, dry_run):
+    print(f"- connecting to {pop3_host} as {username}")
+    server = poplib.POP3_SSL(pop3_host)
+    server.user(username)
+    server.pass_(password)
+    pop3info = server.stat()
+
+    mail_count = pop3info[0]
+    print(f"- downloading {mail_count} message(s)")
+
+    with open(mbox_file, 'w') as mbox_file:
+        indexes = list(reversed(range(mail_count)))
+        with tqdm.tqdm(
+            total=len(indexes),
+            desc="- downloading",
+            bar_format='{l_bar}{bar}| {n_fmt}/{total_fmt} [{elapsed}]',
+        ) as pbar:
+            for i in indexes:
+                if not dry_run:
+                    print(f"From {FROM}", file=mbox_file)
+                    for message in server.retr(i + 1)[1]:
+                        print(message.decode(), file=mbox_file)
+                    print(file=mbox_file)
+                pbar.update(1)
+            if commit:
+                print("- marking messages as read")
+                server.quit()  # prevent marking messages as read
+    print("- done")
+
+
+if __name__ == "__main__":
+    pop3()
